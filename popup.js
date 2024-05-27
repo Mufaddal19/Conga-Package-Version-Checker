b5d70c78bfe8484c4a8fc399b7724b4173e2594d
Muffin1912 <laxmidharmufaddal@gmail.com>
2024-05-27 14:52:56 +0530

    ver1.3

--- popup.js
+++ popup.js
@@ -62,8 +62,19 @@ function getcookie() {
         let viewTriggersButton = document.getElementById("ViewTriggersBtn");
         viewTriggersButton.setAttribute("style", "visibility: visible");
 
+        let viewCallbacksButton = document.getElementById("ViewCallbacksBtn");
+        viewCallbacksButton.setAttribute("style", "visibility: visible");
+
+        let OrgIdText = document.getElementById("OrgId");
+        OrgIdText.setAttribute("style", "visibility: visible");
+
+        let creditText = document.getElementById("Credits");
+        creditText.setAttribute("style", "visibility: visible");
+
+        fetchOrgId();
         fetchPackageFromOrg();
-        fetchTriggersOnAgreementObjectFromOrg();
+        fetchTriggersOnObjectFromOrg();
+        fetchCallbackClasses();
     })
     }
 });
@@ -127,6 +138,23 @@ function getcookie() {
     });
   }
 */
+function fetchOrgId(){
+  console.log("Inside Fetch Org ID Funtion");
+  conn.query(
+    "select Id, PrimaryContact,TrialExpirationDate, OrganizationType, InstanceName, IsSandbox from Organization",
+    function (err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(result);
+      console.log("Got Org Details");
+      let orgId = result.records[0].Id.slice(0,15);
+      var orgValue = document.getElementById("OrgIdValue");
+      orgValue.innerHTML = orgId;
+})
+}
+
+
 function fetchPackageFromOrg() {
   console.log("Inside Fetch Package Funtion");
   conn.tooling.query(
@@ -198,18 +226,28 @@ function fetchPackageFromOrg() {
   );  
 }
 
-function fetchTriggersOnAgreementObjectFromOrg() {
+function fetchTriggersOnObjectFromOrg() {
   // console.log("Ayush Edit starts");
   conn.tooling.query(
     `SELECT Id, Name, TableEnumOrId, ApiVersion, Status
     FROM ApexTrigger
     WHERE TableEnumOrId IN (
+      'Account',
+      'Opportunity',
+      'Apttus_Proposal__Proposal__c',
+      'Apttus_Config2__ProductConfiguration__c',
+      'Apttus_Config2__LineItem__c',
+      'Apttus_Proposal__Proposal_Line_Item__c',
+      'Apttus_Config2__Order__c',
+      'Apttus_Config2__OrderLineItem__c',
+      'Apttus_Config2__SummaryGroup__c',
       'Apttus__APTS_Agreement__c',
       'Apttus__AgreementLineItem__c',
       'Apttus__DocumentVersion__c',
       'Apttus__DocumentVersionDetail__c',
       'Apttus__APTS_Template__c',
-      'Apttus__CycleTimeGroupData__c'
+      'Apttus__CycleTimeGroupData__c',
+      'Apttus_Approval__Approval_Request__c'
     )
     AND NamespacePrefix = null`,
     function (err, result) {
@@ -224,12 +262,22 @@ function fetchTriggersOnAgreementObjectFromOrg() {
 function renderTriggersTable(data) {
   console.log('Inside Render Triggers Table function');
   const objects = {
+    Account: "Account",
+    Opportunity: "Opportunity",
+    Apttus_Proposal__Proposal__c: "Proposal",
+    Apttus_Config2__ProductConfiguration__c: "Product Configuration",
+    Apttus_Config2__LineItem__c: "Line Item",
+    Apttus_Proposal__Proposal_Line_Item__c: "Proposal Line Item",
+    Apttus_Config2__Order__c: "Order",
+    Apttus_Config2__OrderLineItem__c: "Order Line Item",
+    Apttus_Config2__SummaryGroup__c: "Summary Group",
     Apttus__APTS_Agreement__c: "Agreement",
     Apttus__AgreementLineItem__c: "Agreement Line Item",
     Apttus__DocumentVersion__c: "Document Version",
     Apttus__DocumentVersionDetail__c: "Document Version Detail",
     Apttus__APTS_Template__c: "Template",
-    Apttus__CycleTimeGroupData__c: "Cycle Time Group Data"
+    Apttus__CycleTimeGroupData__c: "Cycle Time Group Data",
+    Apttus_Approval__Approval_Request__c: "Approval Request"
   };
   const keys = ['Name', 'TableEnumOrId', 'ApiVersion', 'Status']; // Selected keys
   let table = document.getElementById('triggersTable');
@@ -258,6 +306,124 @@ function renderTriggersTable(data) {
   });
 }
 
+function fetchCallbackClasses() {
+  console.log("fetch Callback Function");
+  conn.query(
+    `select Apttus_Config2__AdjustmentLineItemCallbackClass__c, 
+    Apttus_Config2__AssetLineItemCallbackClass__c, 
+    Apttus_Config2__PricingCallbackClass__c, 
+    Apttus_Config2__ProductAttributeCallbackClass__c, 
+    Apttus_Config2__RelatedPricingCallbackClass__c, 
+    Apttus_Config2__ValidationCallbackClass__c, 
+    Apttus_Config2__DisplayActionCallbackCalss__c, 
+    Apttus_Config2__ProductFilterCallbackClass__c, 
+    Apttus_Config2__AssetRenewalCustomCallbackClass__c, 
+    Apttus_Config2__ActionCallbackClass__c,  
+    Apttus_Config2__OptionFilterCallbackClass__c, 
+    Apttus_Config2__AdjustmentSpreadCallback__c, 
+    Apttus_Config2__PricingExtensionCallbackClass__c, 
+    Apttus_Config2__RevalidationCallbackClass__c from Apttus_Config2__ConfigCustomClasses__c`,
+    function (err, result) {
+      if (err) console.log("Callback Function Errror " + err);
+      // console.log("Success in Ayush Edit: " + result);
+      console.log(result);
+
+      const CallbackClasses = {
+        Apttus_Config2__AdjustmentLineItemCallbackClass__c: result.records[0].Apttus_Config2__AdjustmentLineItemCallbackClass__c,
+        Apttus_Config2__AssetLineItemCallbackClass__c: result.records[0].Apttus_Config2__AssetLineItemCallbackClass__c,
+        Apttus_Config2__PricingCallbackClass__c: result.records[0].Apttus_Config2__PricingCallbackClass__c,
+        Apttus_Config2__ProductAttributeCallbackClass__c: result.records[0].Apttus_Config2__ProductAttributeCallbackClass__c,
+        Apttus_Config2__RelatedPricingCallbackClass__c: result.records[0].Apttus_Config2__RelatedPricingCallbackClass__c,
+        Apttus_Config2__ValidationCallbackClass__c: result.records[0].Apttus_Config2__ValidationCallbackClass__c,
+        Apttus_Config2__DisplayActionCallbackCalss__c: result.records[0].Apttus_Config2__DisplayActionCallbackCalss__c,
+        Apttus_Config2__ProductFilterCallbackClass__c: result.records[0].Apttus_Config2__ProductFilterCallbackClass__c,
+        Apttus_Config2__AssetRenewalCustomCallbackClass__c: result.records[0].Apttus_Config2__AssetRenewalCustomCallbackClass__c,
+        Apttus_Config2__ActionCallbackClass__c: result.records[0].Apttus_Config2__ActionCallbackClass__c,
+        Apttus_Config2__OptionFilterCallbackClass__c: result.records[0].Apttus_Config2__OptionFilterCallbackClass__c,
+        Apttus_Config2__AdjustmentSpreadCallback__c: result.records[0].Apttus_Config2__AdjustmentSpreadCallback__c,
+        Apttus_Config2__PricingExtensionCallbackClass__c: result.records[0].Apttus_Config2__PricingExtensionCallbackClass__c,
+        Apttus_Config2__RevalidationCallbackClass__c: result.records[0].Apttus_Config2__RevalidationCallbackClass__c,
+      };
+      var validCallbackClasses = {};
+      var queryApexClass = "SELECT Id, Name from ApexClass WHERE Name in (";
+      var hasCallbacks = false;
+
+      for (const key in CallbackClasses) {
+        // Check if the value is not null or undefined
+        const value = CallbackClasses[key];
+        if (value !== null && value !== undefined) {
+          // Append the value to queryApexClass with comma separation
+          queryApexClass += `'${value}',`;
+          validCallbackClasses[key] = value;
+          hasCallbacks = true;
+        }
+      }
+      
+      // Remove the trailing comma (if any)
+      queryApexClass = queryApexClass.slice(0, -1);
+      
+      // Close the parentheses for the IN clause
+      queryApexClass += ")";
+      console.log(hasCallbacks);
+      console.log(queryApexClass);
+
+      if(hasCallbacks) fetchCallbackClasses2(queryApexClass,validCallbackClasses);
+  });
+    // renderCallbackTable(result.records);
+}
+
+function fetchCallbackClasses2(query,validClasses){
+  conn.query(query,
+    function (err, result1) {
+      if (err) console.log("Class Function Errror " + err);
+      // console.log("Success in Ayush Edit: " + result);
+
+      renderCallbackTable(result1.records, validClasses);
+});
+}
+
+
+function renderCallbackTable(classData, validClasses) {
+  console.log('Inside Render Callback Table function');
+
+  console.log(classData);
+  console.log(validClasses);
+
+  let table = document.getElementById('CallbackTable');
+  let header = table.createTHead().insertRow();
+  header.insertCell().outerHTML = '<th>Name</th>';
+  header.insertCell().outerHTML = '<th>Apex Class</th>';
+  let body = table.createTBody();
+
+  const keys = ['Name', 'ApexClass']; // Selected keys
+
+  classData.forEach(function(obj) {
+      console.debug(obj);
+      let row = body.insertRow();
+      keys.forEach(function(key) {
+      let cell = row.insertCell();
+      if(key == 'Name') {
+        for (const key1 in validClasses) {
+          if(key1 == "Apttus_Config2__ValidationCallbackClass__c") console.log(validClasses[key1] + " The Obj " + obj.Name);
+          if (validClasses[key1] === obj.Name) {
+            cell.innerHTML = key1;
+            console.log("Value of Key " + key1 + " Value of Class Name " + obj.Name);
+         }
+        }
+      }
+      else if(key == 'ApexClass'){
+        let classLinkElement = document.createElement('a');
+        classLinkElement.href = instanceUrl + '/lightning/setup/ApexClasses/page?address=%2F' + obj.Id;
+        classLinkElement.target = 'blank';
+        classLinkElement.text = obj.Name;
+        cell.appendChild(classLinkElement);
+      }
+  });
+});
+}
+
+
+
 function sendTableToBackground() {
   document.getElementById("btn").addEventListener("click", function () {
     let packagesContainer = document.getElementById("packagesContainer");
@@ -267,6 +433,7 @@ function sendTableToBackground() {
       packagesContainer.style.display = "block";
     }
     document.getElementById("triggersContainer").style.display = "none";
+    document.getElementById("CallbacksContainer").style.display = "none";
     // let searchBar = document.getElementById("myInput");
     // searchBar.setAttribute("style", "visibility: visible");
 
@@ -295,6 +462,7 @@ function sendTableToBackground() {
 
   //View Triggers
   document.getElementById("ViewTriggersBtn").addEventListener("click", ToggleTriggerContainer);
+  document.getElementById("ViewCallbacksBtn").addEventListener("click", ToggleCallbackContainer);
 }
 
 function ToggleTriggerContainer() {
@@ -310,7 +478,21 @@ function ToggleTriggerContainer() {
     display.style.display = "block";
   }
   document.getElementById("packagesContainer").style.display = "none";
+  document.getElementById("CallbacksContainer").style.display = "none";
 }
+
+function ToggleCallbackContainer() {
+  // console.log('inside toggle trigger table function');
+  let display = document.getElementById("CallbacksContainer");
+  if(display.style.display !== "none") {
+    display.style.display = "none";
+  } else {
+    display.style.display = "block";
+  }
+  document.getElementById("packagesContainer").style.display = "none";
+  document.getElementById("triggersContainer").style.display = "none";
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
   var searchInput = document.getElementById("myInput");
@@ -322,11 +504,11 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   if(searchTriggerInput) {
-    searchTriggerInput.addEventListener("keyup", searchTriggers);
+    searchTriggerInput.addEventListener("keyup", () => searchTriggers(false));
   }
 
   if(triggerFilterObject) {
-    triggerFilterObject.addEventListener("input", filterTriggers);
+    triggerFilterObject.addEventListener("input", () => searchTriggers(true));
   }
 
   function myFunction() {
@@ -359,9 +541,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-function searchTriggers() {
-  let input, table, tr, td, td1, td2, td3, i, txtValue1, txtValue2, txtValue3;
-  input = document.getElementById("searchTrigger");
+function searchTriggers(calledFromFilterObject) {
+  let input, table, tr, td, td1, td2, td3, i, txtValue1, txtValue2, txtValue3, txtValue4;
+  calledFromFilterObject ? input = document.getElementById("Object") : input = document.getElementById("searchTrigger");
   searchString = input.value.toUpperCase();
   table = document.getElementById("triggersTable");
   tr = table.getElementsByTagName("tr");
@@ -369,40 +551,21 @@ function searchTriggers() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     console.log("Started");
-    td = tr[i].getElementsByTagName("td")[0]; // Trigger Name 
-    td2 = tr[i].getElementsByTagName("td")[2]; // Trigger API Version
-    td3 = tr[i].getElementsByTagName("td")[3]; // Trigger Status
+    td = tr[i].getElementsByTagName("td")[0];
+    td1 = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    td3 = tr[i].getElementsByTagName("td")[3];
     if (td || td1 || td2 || td3) {
       txtValue1 = td.textContent || td.innerText;
-      txtValue2 = td2.textContent || td2.innerText;
-      txtValue3 = td3.textContent || td3.innerText;
+      txtValue2 = td1.textContent || td1.innerText;
+      txtValue3 = td2.textContent || td2.innerText;
+      txtValue4 = td3.textContent || td3.innerText;
       if (
         txtValue1.toUpperCase().indexOf(searchString) > -1 ||
         txtValue2.toUpperCase().indexOf(searchString) > -1 ||
-        txtValue3.toUpperCase().indexOf(searchString) > -1 
+        txtValue3.toUpperCase().indexOf(searchString) > -1 ||
+        txtValue4.toUpperCase().indexOf(searchString) > -1 
       ) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
-function filterTriggers() {
-  let input, table, tr, td, txtValue1;
-  input = document.getElementById("Object");
-  searchString = input.value.toUpperCase();
-  table = document.getElementById("triggersTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (let i = 0; i < tr.length; i++) {
-    console.log("Started");
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      txtValue1 = td.textContent || td.innerText;
-      if (searchString == "" || txtValue1.toUpperCase() === searchString) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
@@ -431,4 +594,35 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+  const CallbackClasses = {
+    Apttus_Config2__AdjustmentLineItemCallbackClass__c: "Adjustment Line Item Callback Class",
+    Apttus_Config2__AssetLineItemCallbackClass__c: "Asset Line Item Callback Class",
+    Apttus_Config2__PricingCallbackClass__c: "Pricing Callback Class",
+    Apttus_Config2__ProductAttributeCallbackClass__c: "Product Attribute Callback Class",
+    Apttus_Config2__RelatedPricingCallbackClass__c: "Related Pricing Callback Class",
+    Apttus_Config2__ValidationCallbackClass__c: "Validation Callback Class",
+    Apttus_Config2__DisplayActionCallbackCalss__c: "Display Action Callback Class",
+    Apttus_Config2__ProductFilterCallbackClass__c: "Product Filter Callback Class",
+    Apttus_Config2__AssetRenewalCustomCallbackClass__c: "Asset Renewal Custom Callback Class",
+    Apttus_Config2__ActionCallbackClass__c: "Action Callback Class",
+    Apttus_Config2__OptionFilterCallbackClass__c: "Option Filter Callback Class",
+    Apttus_Config2__AdjustmentSpreadCallback__c: "Adjustment Spread Callback",
+    Apttus_Config2__PricingExtensionCallbackClass__c: "Pricing Extension Callback Class",
+    Apttus_Config2__RevalidationCallbackClass__c: "Revalidation Callback Class",
+  };
+  const ClassList = [
+  'Apttus_Config2__AdjustmentLineItemCallbackClass__c', 
+  'Apttus_Config2__AssetLineItemCallbackClass__c',
+  'Apttus_Config2__PricingCallbackClass__c',
+  'Apttus_Config2__ProductAttributeCallbackClass__c',
+  'Apttus_Config2__RelatedPricingCallbackClass__c', 
+  'Apttus_Config2__ValidationCallbackClass__c',
+  'Apttus_Config2__DisplayActionCallbackCalss__c',
+  'Apttus_Config2__ProductFilterCallbackClass__c',
+  'Apttus_Config2__AssetRenewalCustomCallbackClass__c',
+  'Apttus_Config2__ActionCallbackClass__c',
+  'Apttus_Config2__OptionFilterCallbackClass__c', 
+  'Apttus_Config2__AdjustmentSpreadCallback__c',
+  'Apttus_Config2__PricingExtensionCallbackClass__c',
+  'Apttus_Config2__RevalidationCallbackClass__c']; // Selected keys
 */
